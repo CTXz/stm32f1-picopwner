d57813e4bff07825f00744d69d1bcbcf1c3e4f3d
Patrick Pedersen <ctx.xda@gmail.com>
2024-02-24 00:31:01 +0100

    GPLv3: Updated copyright year to 2024
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- attack/attack.c
+++ attack/attack.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Patrick Pedersen
+ * Copyright (C) 2024 Patrick Pedersen
 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
